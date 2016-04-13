2174664f988f37982df1e4a59234a2d0a4b444d6
Tero Kontkanen <tero.kontkanen@granitedevices.fi>
2016-04-13 18:32:30 +0300

    Updated version code to 2000 for final 2.0.0 firmware

--- src/System.h
+++ src/System.h
@@ -63,7 +63,7 @@
  * -serial comm fails sometimes after FW upgrade and app launch from granity. perhaps address goes wrong or it gets disturbed by serial comm rx too early?
  *
  */
-#define FW_VERSION 1095
+#define FW_VERSION 2000
 #define FW_BACKWARDS_COMPATITBLE_VERSION 1090
 
 #define COMMAND_QUEUE1_SIZE 256
